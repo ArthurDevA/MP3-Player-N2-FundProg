79435c2a53172005d0cc7b624a953877c7214d68
Arthur <arthurdearaujocustodio0@gmail.com>
2023-12-10 03:27:15 -0300

    lista no js

---
+++ script.js
@@ -0,0 +1,23 @@
+const musicas = [
+    "uiratsupu-quan-shan-tamakiniku-qing-woru-reruratsupu.mp3",
+    "zhen-shigureuitotemotsuyoigao-zhi-pv.mp3",
+    "adrenaline-trysail-covered-by-shigureui-da-kong-subaru.mp3",
+    "qun-qing-yoasobi-covered-by-shigureui.mp3",
+    "debiruziyanaimon-covered-by-shigureui-jiao-juan-watame.mp3",
+    "daishikiyudaishiyuki-covered-by-shigureui-tian-gong-kokoro.mp3",
+    "orizinarule-qu-rainy-lady-shigureui.mp3",
+    "orizinarule-qu-su-sheng-rorishen-rekuiemu-shigureui-9sai.mp3",
+    "shiyanimasu-sos-covered-by-shigureui.mp3"
+]
+
+function createSongList() {
+    const list = document.createElement("ol")
+    for (let i = 0; i < musicas.length; i++) {
+        const item = document.createElement("li")
+        item.appendChild(document.createTextNode(musicas[i]))
+        list.appendChild(item)
+    }
+    return list
+}
+
+document.getElementById('listaMusicas').appendChild(createSongList())
